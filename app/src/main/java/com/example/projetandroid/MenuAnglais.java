6e198bca1e0f2cc89f2bb4ea935679bff526c892
Rémy <remy.albanese2@gmail.com>
2022-05-30 14:57:40 +0200

    AJOUT DU MENU ANGLAIS ET GEOGRAPHIE DE FACON TRES RAPIDE EN CTRL C CTRL V MAIS CA MARCHE (avec des jambes d'ailleurs, tu l'as ? moi je l'ai)

--- app/src/main/java/com/example/projetandroid/MenuAnglais.java
+++ app/src/main/java/com/example/projetandroid/MenuAnglais.java
@@ -1,16 +1,16 @@
 package com.example.projetandroid;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 
-public class MenuMaths extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MenuAnglais extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu_maths);
+        setContentView(R.layout.activity_menu_anglais);
     }
 
     public void retour (View view){
--- app/src/main/java/com/example/projetandroid/MenuGeographie.java
+++ app/src/main/java/com/example/projetandroid/MenuGeographie.java
@@ -1,16 +1,16 @@
 package com.example.projetandroid;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 
-public class MenuMaths extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MenuGeographie extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu_maths);
+        setContentView(R.layout.activity_menu_geographie);
     }
 
     public void retour (View view){
