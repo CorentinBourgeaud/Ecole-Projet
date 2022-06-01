2a263a18c064da668732f9ede54fafd40eac69c6
Corentin <bourgeac@etu.univ-grenoble-alpes.fr>
2022-06-01 16:56:57 +0200

    recup db

---
+++ app/src/main/java/com/example/Activités/TeteContreTable.java
@@ -0,0 +1,19 @@
+package com.example.Activités;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.example.projetandroid.R;
+
+public class TeteContreTable extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tete_contre_table);
+    }
+}
