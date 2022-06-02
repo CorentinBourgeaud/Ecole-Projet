b817105a2e341f7cf0f3b7ac7991fcca64b7fb31
Corentin <bourgeac@etu.univ-grenoble-alpes.fr>
2022-06-02 12:29:26 +0200

    exercice tete table

--- app/src/main/java/com/example/Activités/ResultatTeteContreTable.java
+++ app/src/main/java/com/example/Activités/ResultatTeteContreTable.java
@@ -18,4 +18,14 @@ public class ResultatTeteContreTable extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultat_tete_contre_table);
     }
+
+    public void changeChoixTeteContreTable(View view) {
+        Intent intent = new Intent(this, ChoixTeteContreTable.class);
+        startActivity(intent);
+    }
+
+    public void changeMaths(View view) {
+        Intent intent = new Intent(this, MenuMaths.class);
+        startActivity(intent);
+    }
 }
